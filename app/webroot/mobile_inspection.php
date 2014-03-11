8c7442b640181886f8bb86a4532dc4390e8a24f7
van trin <info@trinoweb.com>
2014-03-11 22:02:48 +0545

    changes

--- app/webroot/mobile_inspection.php
+++ app/webroot/mobile_inspection.php
@@ -28,19 +28,19 @@
         <tr>
         <td rowspan="3">Address:</td>
         <td rowspan="3"><textarea name="mobile_ins[address]" rows="5" class="required"><?php if(isset($mobins)) echo $mobins['MobileInspection']['address'];?></textarea></td>
-        <td><input type="text" style="width: 150px;" name="mobile_ins[time_arrived1]" placeholder="HH:MM" value="<?php if(isset($mobins) && $mobins['MobileInspection']['time_arrived1']){echo $mobins['MobileInspection']['time_arrived1'];}?>" /></td>
-        <td><input type="text" style="width: 150px;" name="mobile_ins[time_departed1]" placeholder="HH:MM" value="<?php if(isset($mobins) && $mobins['MobileInspection']['time_departed1']){echo $mobins['MobileInspection']['time_departed1'];}?>" /></td>
+        <td><input type="text" class="time" style="width: 150px;" name="mobile_ins[time_arrived1]" placeholder="HH:MM" value="<?php if(isset($mobins) && $mobins['MobileInspection']['time_arrived1']){echo $mobins['MobileInspection']['time_arrived1'];}?>" /></td>
+        <td><input type="text" class="time" style="width: 150px;" name="mobile_ins[time_departed1]" placeholder="HH:MM" value="<?php if(isset($mobins) && $mobins['MobileInspection']['time_departed1']){echo $mobins['MobileInspection']['time_departed1'];}?>" /></td>
         </tr>
 
-        <tr><td><input type="text" style="width: 150px;" name="mobile_ins[time_arrived2]" placeholder="HH:MM" value="<?php if(isset($mobins) && $mobins['MobileInspection']['time_arrived2']){echo $mobins['MobileInspection']['time_arrived2'];}?>" /></td>
-        <td><input type="text" style="width: 150px;" name="mobile_ins[time_departed2]" placeholder="HH:MM" value="<?php if(isset($mobins) && $mobins['MobileInspection']['time_departed2']){echo $mobins['MobileInspection']['time_departed2'];}?>" /></td></tr>
-        <tr><td><input type="text" style="width: 150px;" name="mobile_ins[time_arrived3]" placeholder="HH:MM" value="<?php if(isset($mobins) && $mobins['MobileInspection']['time_arrived3']){echo $mobins['MobileInspection']['time_arrived3'];}?>"/></td>
-        <td><input type="text" style="width: 150px;" name="mobile_ins[time_departed3]" placeholder="HH:MM" value="<?php if(isset($mobins) && $mobins['MobileInspection']['time_departed3']){echo $mobins['MobileInspection']['time_departed3'];}?>"/></td></tr>
+        <tr><td><input type="text" class="time" style="width: 150px;" name="mobile_ins[time_arrived2]" placeholder="HH:MM" value="<?php if(isset($mobins) && $mobins['MobileInspection']['time_arrived2']){echo $mobins['MobileInspection']['time_arrived2'];}?>" /></td>
+        <td><input type="text" class="time" style="width: 150px;" name="mobile_ins[time_departed2]" placeholder="HH:MM" value="<?php if(isset($mobins) && $mobins['MobileInspection']['time_departed2']){echo $mobins['MobileInspection']['time_departed2'];}?>" /></td></tr>
+        <tr><td><input type="text" class="time" style="width: 150px;" name="mobile_ins[time_arrived3]" placeholder="HH:MM" value="<?php if(isset($mobins) && $mobins['MobileInspection']['time_arrived3']){echo $mobins['MobileInspection']['time_arrived3'];}?>"/></td>
+        <td><input type="text" class="time" style="width: 150px;" name="mobile_ins[time_departed3]" placeholder="HH:MM" value="<?php if(isset($mobins) && $mobins['MobileInspection']['time_departed3']){echo $mobins['MobileInspection']['time_departed3'];}?>"/></td></tr>
         <tr><td>P.O. No.: <input type="text" style="width: 150px;" name="mobile_ins[pono]" class="required" value="<?php if(isset($mobins) && $mobins['MobileInspection']['pono']){echo $mobins['MobileInspection']['pono'];}?>" /></td>
         <td>Transit/Location No. <input type="text" style="width: 150px;" name="mobile_ins[transitno]" class="required" value="<?php if(isset($mobins) && $mobins['MobileInspection']['transitno']){echo $mobins['MobileInspection']['transitno'];}?>" /></td>
         <td>File Key No. <input type="text" style="width: 150px;" name="mobile_ins[filekeyno]" class="required" value="<?php if(isset($mobins) && $mobins['MobileInspection']['filekeyno']){echo $mobins['MobileInspection']['filekeyno'];}?>" /></td>
-        <td><input type="text" style="width: 150px;" name="mobile_ins[time_arrived4]" class="required" placeholder="HH:MM" value="<?php if(isset($mobins) && $mobins['MobileInspection']['date']){echo $mobins['MobileInspection']['time_arrived4'];}?>"/></td>
-        <td><input type="text" style="width: 150px;" name="mobile_ins[time_departed4]" class="required" placeholder="HH:MM" value="<?php if(isset($mobins) && $mobins['MobileInspection']['time_departed4']){echo $mobins['MobileInspection']['time_departed4'];}?>"/></td></tr>
+        <td><input type="text" class="time" style="width: 150px;" name="mobile_ins[time_arrived4]"  placeholder="HH:MM" value="<?php if(isset($mobins) && $mobins['MobileInspection']['date']){echo $mobins['MobileInspection']['time_arrived4'];}?>"/></td>
+        <td><input type="text" class="time" style="width: 150px;" name="mobile_ins[time_departed4]"  placeholder="HH:MM" value="<?php if(isset($mobins) && $mobins['MobileInspection']['time_departed4']){echo $mobins['MobileInspection']['time_departed4'];}?>"/></td></tr>
 
         </table>
         <br />
@@ -55,7 +55,7 @@
             {
         ?>
                 
-                <tr><td><input type="text" name="mobtime[]" value="<?php echo $action['MobileAction']['time'];?>"/></td>
+                <tr><td><input type="text" class="time" name="mobtime[]" value="<?php echo $action['MobileAction']['time'];?>"/></td>
                 <td><textarea name="mobdetail[]" ><?php echo $action['MobileAction']['detail'];?></textarea></td>
                 <td><a href='javascript:void(0)' onclick='$(this).closest("tr").remove();' id="delete" class='btn btn-danger'>Delete</a></td></tr>
                 
@@ -63,16 +63,23 @@
             }
             
         }?>
-        <tr class="mobtime" ><td><input type="text" name="mobtime[]" />
+        <tr class="mobtime" ><td><input type="text" class="time" name="mobtime[]" />
         </td><td><textarea name="mobdetail[]"></textarea></td><td><input type="button" class="btn btn-primary" id="addmore" value="+Add More"/></td></tr >
         
+        </table>
+        <table>
+        <thead><th>Operative Name:</th><th>Licence No:</th><th>Vehicle No:</th></thead>
+        <tr><td><input type="text" name="mobile_ins[operative_name]" class="required"  value="<?php if(isset($mobins) && $mobins['MobileInspection']['operative_name']){echo $mobins['MobileInspection']['operative_name'];}?>" /></td>
+        <td><input type="text" name="mobile_ins[licence_no]" class="required"  value="<?php if(isset($mobins) && $mobins['MobileInspection']['licence_no']){echo $mobins['MobileInspection']['licence_no'];}?>" /></td>
+        <td><input type="text" name="mobile_ins[vehicle_no]" class="required"  value="<?php if(isset($mobins) && $mobins['MobileInspection']['vehicle_no']){echo $mobins['MobileInspection']['vehicle_no'];}?>" /></td>
+        </tr>
         </table>
 </td>            
 </tr>
 <script>
 $(function(){
     $('#addmore').click(function(){
-       $("<tr><td><input type='text' name='mobtime[]' /></td><td><textarea name='mobdetail[]'></textarea></td><td><a href='javascript:void(0)' onclick='$(this).closest(\"tr\").remove();' class='btn btn-danger'>Delete</a></td></tr>").insertAfter($('.mobtime')); 
+       $("<tr><td><input type='text' class='time' name='mobtime[]' /></td><td><textarea name='mobdetail[]'></textarea></td><td><a href='javascript:void(0)' onclick='$(this).closest(\"tr\").remove();' class='btn btn-danger'>Delete</a></td></tr>").insertAfter($('.mobtime')); 
     });
    <?php if($this->params['action'] == 'view_detail' ){ ?> 
     $('.mobileins_more').show();
@@ -80,6 +87,8 @@ $(function(){
     $('.mobileins_more textarea').attr('disabled','disabled');
     $('.mobtime , #delete').hide();
    <?php } ?> 
+   
+   $('.time').timepicker();
 });
 </script>
 <style>input[type="radio"]{margin-top:0;}</style>
