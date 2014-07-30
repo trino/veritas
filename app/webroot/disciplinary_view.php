23ec49f9e1633d87579d91ba7d5528c47500619b
unknown <justdoit2045@gmail.com>
2014-07-31 01:13:01 +0545

    test

--- app/webroot/disciplinary_view.php
+++ app/webroot/disciplinary_view.php
@@ -10,28 +10,28 @@
 $rules = $dw['Dispilinary']['rule'];
 $r_arr = explode(',',$rules);
 ?>
-<tr><td><?php if(in_array('1',$r_arr)){?>&#10004<?php }else echo "&times;";?> Voilation of Work Rules</td></tr>
-<tr><td><?php if(in_array('2',$r_arr)){?>&#10004<?php }else echo "&times;";?> Violation of Company Policy</td></tr>
-<tr><td><?php if(in_array('3',$r_arr)){?>&#10004<?php }else echo "&times;";?> Violation of Safety Rules</td></tr>
-<tr><td><?php if(in_array('4',$r_arr)){?>&#10004<?php }else echo "&times;";?> Insubordination</td></tr>
-<tr><td><?php if(in_array('5',$r_arr)){?>&#10004<?php }else echo "&times;";?> Under the influence of drugs/alcohol or in possessions of drugs/alcohol</td></tr>
-<tr><td><?php if(in_array('6',$r_arr)){?>&#10004<?php }else echo "&times;";?> Discourtesy or verbal abuse of guest or other employee</td></tr>
-<tr><td><?php if(in_array('7',$r_arr)){?>&#10004<?php }else echo "&times;";?> Damage to client/company property</td></tr>
-<tr><td><?php if(in_array('8',$r_arr)){?>&#10004<?php }else echo "&times;";?> Physical or Verbal assault and/or fighting</td></tr>
-<tr><td><strong>Other :</strong> <?php if(isset($dw['Dispilinary']['other']))echo $dw['Dispilinary']['other'];?></td></tr>
+<tr><td colspan="4"><?php if(in_array('1',$r_arr)){?>&#10004<?php }else echo "&times;";?> Voilation of Work Rules</td></tr>
+<tr><td colspan="4"><?php if(in_array('2',$r_arr)){?>&#10004<?php }else echo "&times;";?> Violation of Company Policy</td></tr>
+<tr><td colspan="4"><?php if(in_array('3',$r_arr)){?>&#10004<?php }else echo "&times;";?> Violation of Safety Rules</td></tr>
+<tr><td colspan="4"><?php if(in_array('4',$r_arr)){?>&#10004<?php }else echo "&times;";?> Insubordination</td></tr>
+<tr><td colspan="4"><?php if(in_array('5',$r_arr)){?>&#10004<?php }else echo "&times;";?> Under the influence of drugs/alcohol or in possessions of drugs/alcohol</td></tr>
+<tr><td colspan="4"><?php if(in_array('6',$r_arr)){?>&#10004<?php }else echo "&times;";?> Discourtesy or verbal abuse of guest or other employee</td></tr>
+<tr><td colspan="4"><?php if(in_array('7',$r_arr)){?>&#10004<?php }else echo "&times;";?> Damage to client/company property</td></tr>
+<tr><td colspan="4"><?php if(in_array('8',$r_arr)){?>&#10004<?php }else echo "&times;";?> Physical or Verbal assault and/or fighting</td></tr>
+<tr><td colspan="4"><strong>Other :</strong> <?php if(isset($dw['Dispilinary']['other']))echo $dw['Dispilinary']['other'];?></td></tr>
 <tr><td colspan="4"><strong>Type Of Warning</strong></td></tr>
 <?php
 $warn = $dw['Dispilinary']['warning'];
 $w_arr = explode(',',$warn);
 ?>
-<tr><td><?php if(in_array('1',$w_arr)){?>&#10004<?php }else echo "&times;";?> Verbal Warning</td></tr>
-<tr><td><?php if(in_array('2',$w_arr)){?>&#10004<?php }else echo "&times;";?> Written Warning</td></tr>
-<tr><td><?php if(in_array('3',$w_arr)){?>&#10004<?php }else echo "&times;";?> Final Written Warning</td></tr>
-<tr><td><?php if(in_array('4',$w_arr)){?>&#10004<?php }else echo "&times;";?> Termination </td></tr>
-<tr><td><strong>Supervisor/Manager Comments</strong></td></tr>
-<tr><td><?php if(isset($dw['Dispilinary'])) echo $dw['Dispilinary']['supervisor_comment'];?></td></tr>
-<tr><td><strong>Employee Comments</strong></td></tr>
-<tr><td><?php if(isset($dw['Dispilinary'])) echo $dw['Dispilinary']['employee_comment'];?></td></tr>
+<tr><td colspan="4"><?php if(in_array('1',$w_arr)){?>&#10004<?php }else echo "&times;";?> Verbal Warning</td></tr>
+<tr><td colspan="4"><?php if(in_array('2',$w_arr)){?>&#10004<?php }else echo "&times;";?> Written Warning</td></tr>
+<tr><td colspan="4"><?php if(in_array('3',$w_arr)){?>&#10004<?php }else echo "&times;";?> Final Written Warning</td></tr>
+<tr><td colspan="4"><?php if(in_array('4',$w_arr)){?>&#10004<?php }else echo "&times;";?> Termination </td></tr>
+<tr><td colspan="4"><strong>Supervisor/Manager Comments</strong></td></tr>
+<tr><td colspan="4"><?php if(isset($dw['Dispilinary'])) echo $dw['Dispilinary']['supervisor_comment'];?></td></tr>
+<tr><td colspan="4"><strong>Employee Comments</strong></td></tr>
+<tr><td colspan="4"><?php if(isset($dw['Dispilinary'])) echo $dw['Dispilinary']['employee_comment'];?></td></tr>
 
 </table>
 <div style="position: relative;padding:5px;">
