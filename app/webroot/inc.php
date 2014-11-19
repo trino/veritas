1d78e3d31beeec96964c5ab5a22b5031a19927c6
van <info@trinoweb.com>
2014-11-19 12:55:05 -0500

    ad

--- app/webroot/inc.php
+++ app/webroot/inc.php
@@ -1,9 +1,7 @@
 <?php
-if($_SERVER['SERVER_NAME']=='localhost' )
+if($_SERVER['SERVER_NAME']=='localhost')
     $base_url = "http://localhost/veritas/";
-elseif($_SERVER['SERVER_NAME']='192.168.1.100')
-    $base_url = "http://192.168.1.100/veritas/";
 else
-    $base_url ="http://bigcitymovers.ca/";
+    $base_url ="http://afimacus-veritas.com/";
     include_once('functions.php');
 ?>
